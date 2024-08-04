fbdedf5bc9dc89e6e2003bca840a070672278f6f
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-04 21:08:49 +0300

    cache added

--- src/main/java/com/test/redis/service/UserService.java
+++ src/main/java/com/test/redis/service/UserService.java
@@ -6,6 +6,9 @@ import com.test.redis.model.User;
 import com.test.redis.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,19 +21,23 @@ public class UserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
 
+    @CacheEvict(value = {"users"}, allEntries = true)
     public User createUser(CreateUserRequest request) {
         return userRepository.save(modelMapper.map(request, User.class));
     }
 
+    @Cacheable(value = "users", key = "#root.methodName", unless = "#result == null")
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    @Cacheable(cacheNames = "user_id", key = "#root.methodName + #id", unless = "#result == null")
     public User getUserById(Long id) {
         return userRepository.findById(id)
                 .orElse(null);
     }
 
+    @CachePut(cacheNames = "user_id", key = "'getUserById' + #request.id", unless = "#result == null")
     public User updateUser(UpdateUserRequest request) {
         Optional<User> entity = userRepository.findById(request.id());
 
@@ -44,6 +51,7 @@ public class UserService {
         }
     }
 
+    @CacheEvict(value = {"users", "user_id"}, allEntries = true)
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
     }
