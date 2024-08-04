536d746322382ce459da6efee48da242209ea986
Aziz Can Öz <oz.azizcann@gmail.com>
2024-08-04 21:03:44 +0300

    model mapper bean added

--- src/main/java/com/test/redis/RedisApplication.java
+++ src/main/java/com/test/redis/RedisApplication.java
@@ -1,13 +1,23 @@
 package com.test.redis;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.record.RecordModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.registerModule(new RecordModule());
+        return modelMapper;
     }
 
 }
